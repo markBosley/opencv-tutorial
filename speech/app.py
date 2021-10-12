80ca2cc48ebdaaf076458f9c6a701df6084fe4bd
markBosley <mark.bosley@brikeda.com>
2021-10-12 13:59:08 -0700

    fixed speech

---
+++ speech/app.py
@@ -0,0 +1,30 @@
+from time import sleep
+import speech_recognition as sr
+import pyttsx3
+
+recognizer = sr.Recognizer()
+
+while  True:
+    try:
+        with sr.Microphone() as mic:            
+            recognizer.adjust_for_ambient_noise(mic, duration =0.2)
+            audio = recognizer.listen(mic)
+            text=recognizer.recognize_google(audio)
+            text=text.lower()
+            print(f"Recognized:{text}")
+    except sr.UnknonValueError():
+        recognizer = sr.Recognizer()
+
+        continue
+
+
+
+# the program will print hello world
+#  every 1 second foever
+"""
+while True:
+    print("Hello, crule World")
+    sleep(1)
+"""
+
+print("the fro like the boiling water")
